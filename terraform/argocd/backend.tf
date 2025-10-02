6cb59789c54f7032ba277f40b4ba019c21169990
sergii-edu <157320532+sergii-edu@users.noreply.github.com>
2025-10-03 00:49:50 +0300

    Delete terraform/argocd directory

--- terraform/argocd/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    # ⚠️ UPDATE to your bucket/region/profile
-    bucket  = "mlops-terraform-goit"
-    key     = "argocd/terraform.tfstate"
-    region  = "us-east-1"
-    profile = "goit-terraform"
-  }
-}
